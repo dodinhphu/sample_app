6204a8a1d38a9d10801dfee0521f74c028083ef9
Do Dinh Phu <dodinhphu316@gmail.com>
2022-03-28 08:57:00 +0700

    static page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  scope "(:locale)", locale: /en|vi/ do
+    root "static_pages#home"
+    get "/help", to:"static_pages#help"
+    get "/about", to:"static_pages#about"
+    get "/contact", to:"static_pages#contact"
+    get "/signup", to: "users#new"
+  end
 end
