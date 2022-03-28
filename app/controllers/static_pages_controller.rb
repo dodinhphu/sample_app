c8246ffc54c966112c5a0b0a2b748b3630de75eb
Do Dinh Phu <dodinhphu316@gmail.com>
2022-03-28 08:57:00 +0700

    chapter-13

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,10 @@
 class StaticPagesController < ApplicationController
-  def home; end
+  def home
+    return unless logged_in?
+
+    @micropost = current_user.microposts.build
+    @pagy, @feed_items = pagy current_user.microposts
+  end
 
   def help; end
 
